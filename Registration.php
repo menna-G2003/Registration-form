1e0533eaf970318b496414d91865abae16f7c50c
menna ghazy <150367283+menna-G2003@users.noreply.github.com>
2024-12-21 19:55:16 +0200

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,99 @@
+<!-- دا فرونت الفورم  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title> Registration Form </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+  <h2> Registration Form </h2>
+  <br>
+  <!-- Receives errors that I have reviewed from the back-end -->
+  <?php
+  if (isset($_GET['errors']))
+    $errors = json_decode($_GET['errors'], true); // conversion data from array to string
+  ?>
+  <form action="done.php" method="post">
+    <label for="first_name "> first name </label>
+    <input type="text" id="first_name" name="first_name">
+    <br>
+    <br>
+    <label for="last_name "> last name </label>
+    <input type="text" id="last_name" name="last_name">
+    <br>
+    <br>
+    <label for="address"> address </label>
+    <textarea rows="5" cols="30" name="address" placeholder=" add Your address"></textarea>
+    <br>
+    <br>
+    <label for="country">country </label>
+    <select name="country">
+      <option value="select country" selected> select country </option>
+      <option value="cairo">cairo</option>
+      <option value="giza"> giza</option>
+    </select>
+    <br>
+    <br>
+    <label for="gender"> gender
+      <label for="male">
+        <input type="radio" id="male" name="male" value="male"> male
+      </label>
+      <label for="female">
+        <input type="radio" id="female" name="male" value="female"> female
+      </label>
+      <br>
+      <br>
+      <form>
+        <label for="skills" name="skills[]" value="skills"> skills </label><br>
+        <label for="php">
+          <input type="checkbox" id="php" name="skills[]" value="php"> php
+        </label>
+        <label for="My SQL">
+          <input type="checkbox" id="My SQL" name="skills[]" value="My SQL"> My SQL <br>
+        </label>
+        <label for="c++">
+          <input type="checkbox" id="c++" name="skills[]" value="c++">c++
+        </label>
+        <label for="css">
+          <input type="checkbox" id="css" name="skills[]" value="css">css<br>
+        </label>
+      </form>
+      <br>
+      <br>
+      <!-- دا هعتبره email -->
+      <label for="user name "> user name </label>
+      <input type="text" id="user name" name="user_name">
+      <!-- Show Error Messages to display a error message When the user enters the wrong username -->
+      <small class="text-danger">
+        <?php
+        if (isset($errors['user_name']))
+          echo $errors['user_name'];
+        ?>
+      </small>
+      <br>
+      <br>
+      <label for="password">password</label>
+      <input type="password" id="password" name="password">
+      <!-- Show Error Messages to display a error message When the user enters the wrong password -->
+      <small class="text-danger">
+        <?php
+        if (isset($errors['password']))
+          echo $errors['password'] ?>
+      </small>
+      <br>
+      <br>
+      <label for="department">department</label>
+      <input type="text" id="department" name="department" placeholder="open source">
+      <br>
+      <br>
+      <input type="submit" value="Submit">
+      <input type="reset" value="Reset">
+      <br>
+      <br>
+  </form>
+</body>
+
+</html>
---
+++ form.php
@@ -0,0 +1,59 @@
+<!-- دا باك الفورم  -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Review Your Information</title>
+</head>
+<body>
+    <h1>Review Your Information</h1>
+    <?php 
+       $first_name = $_POST['first_name'];
+       $last_name = $_POST['last_name'];
+       $fullName = $first_name . " " . $last_name; 
+      $address = $_POST['address']; 
+      $skills = $_POST['skills'];
+      $department = $_POST['department'];
+      //check gender and set Result
+      $gender = "male"; 
+      $salutation = ($gender == "male") ? "Miss." : "Mr.";
+      // Verification of user name 
+      $user_name=$_POST["user_name"];
+      $problem =[];                  //array because may be have errors 
+      if(isset($_POST['user_name']) && !empty($_POST['user_name']))
+        {
+           $user_name = $_POST['user_name'];
+           echo "";
+        }
+     else
+         $errors['user_name'] = "Invalid username, please enter it again";
+
+       // Verification of pssword 
+       if(isset($_POST['password']) && !empty($_POST['password']))
+        {
+           $password = $_POST['password'];
+           echo "";
+        }
+     else
+         $errors['password'] = "Invalid password, please enter it again";
+
+    //This code checks for errors 
+         if(!empty($errors))
+        {
+         $errors = json_encode($errors); // conversion data from array to string
+
+        //There is a problem, go back to this page
+         header('Location:rej.php?errors='.$errors);
+        }
+    
+    ?>
+    <p>Thanks <?php echo $salutation; ?> <?php echo $fullName; ?></p>
+    <p>please Review your Information</p>
+    <p><strong>Name:</strong> <?php echo $fullName ; ?></p>
+    <p><strong>address:</strong> <?php echo $address ; ?></p>
+    <p><strong>Department:</strong> <?php echo $department; ?></p>
+    <p><strong>Your Skills:</strong></p>
+    <?php
+   foreach ($skills as $skill) {
+    echo $skill . "<br>"; } ?>
+</body>
+</html>
